6840d1751b0fc7611f828f59df19bbc6de475192
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-09-06 01:41:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ takeANumber(katzDeliLine, "Steven"); // "Welcome, Ada. You are number 1 in line.
 takeANumber(katzDeliLine, "Blake"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Avi"); // "Welcome, Kent. You are number 3 in line."
 
+<<<<<<< HEAD
 function currentLine(x) {
     var line = []
     if (x.length === 0) {
@@ -18,6 +19,18 @@ function currentLine(x) {
       }
       line = line.slice(0, line.length-2)
       return "The line is currently: " + line
+=======
+function currentLine (katzDeliLine) {
+  var line = []
+  if (katzDeliLine.length ===0) {
+    return "The line is currently empty."
+  } else {
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    line += (i + 1) + ". " + katzDeliLine[i] + ","
+  }
+  line = line.slice ( 0,  line.length -1)
+  return "The line is currently:  " + line
+>>>>>>> 0cfb0cee742e0415d01cd06db5d2b05d9e408b08
 }
 }
 function nowServing(x) {
